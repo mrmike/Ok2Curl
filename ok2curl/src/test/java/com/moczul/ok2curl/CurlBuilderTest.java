4a9d47c6cb6846a75cceef3d8a4089f1ab91f11e
Michal Moczulski <moczul@gmail.com>
2016-11-15 21:00:57 +0100

    Handle multiple headers with the same name

--- ok2curl/src/test/java/com/moczul/ok2curl/CurlBuilderTest.java
+++ ok2curl/src/test/java/com/moczul/ok2curl/CurlBuilderTest.java
@@ -68,6 +68,19 @@ public class CurlBuilderTest {
         assertEquals(expected, command);
     }
 
+    @Test
+    public void multipleHeadersWithTheSameName_shouldBeAddedToCurlCommand() {
+        final Request request = new Request.Builder()
+                .url("http://example.com/")
+                .addHeader("Cookie", "FIRST=foo")
+                .addHeader("Cookie", "SECOND=bar")
+                .build();
+
+        final String command = new CurlBuilder(request).build();
+
+        assertEquals("curl -X GET -H \"Cookie:FIRST=foo\" -H \"Cookie:SECOND=bar\" http://example.com/", command);
+    }
+
     private RequestBody body() {
         return new FormBody.Builder().add("key1", "value1").build();
     }
