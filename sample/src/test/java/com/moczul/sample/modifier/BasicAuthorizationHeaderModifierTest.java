a1a364d09063bf83305262ba01a18f2d368a4a33
Frieder Bluemle <frieder.bluemle@gmail.com>
2018-10-03 15:34:09 +0800

    Update mockito to 2.22.0

--- sample/src/test/java/com/moczul/sample/modifier/BasicAuthorizationHeaderModifierTest.java
+++ sample/src/test/java/com/moczul/sample/modifier/BasicAuthorizationHeaderModifierTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -14,7 +14,6 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BasicAuthorizationHeaderModifierTest {
-
     @Mock
     Base64Decoder base64Decoder;
     @InjectMocks
